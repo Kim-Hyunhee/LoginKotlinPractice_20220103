d3801b4c8c4652fe40c0516475101234e3373077
김현희 <tta_bi@naver.com>
2022-01-03 16:59:28 +0900

    버튼 눌리면 -> 입력한 아이디 / 비번 받아오기 - 변수에 저장

--- app/src/main/java/com/nepplus/loginkotlinpractice_20220103/MainActivity.kt
+++ app/src/main/java/com/nepplus/loginkotlinpractice_20220103/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.loginkotlinpractice_20220103
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,9 +11,19 @@ class MainActivity : AppCompatActivity() {
 
         //  로그인 버튼이 눌리면 할일
 
-        // 1. 입력한 아이디와 비번이 뭔지 변수에 담아두자
+        btnLogin.setOnClickListener {
 
-        // 2. id : admin / pw : qwer1234 두 개가 모두 맞다면 토스트로 "관리자입니다."
-        //  => 둘 중 하나라도 틀리다면 "로그인 실패" 토스트
+            // 1. 입력한 아이디와 비번이 뭔지 변수에 담아두자
+
+            val inputId = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+
+            // 2. id : admin / pw : qwer1234 두 개가 모두 맞다면 토스트로 "관리자입니다."
+            //  => 둘 중 하나라도 틀리다면 "로그인 실패" 토스트
+
+
+        }
+
+
     }
 }
